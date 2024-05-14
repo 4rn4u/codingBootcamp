c2367f32bcc0f71fcbde5db041899d401c082a6c
4rn4u <vilx00@protonmail.com>
2024-05-14 09:26:36 +0200

    a

--- week1/pE.java
+++ week1/pE.java
@@ -5,17 +5,19 @@ import java.util.*;;
 public class pE {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int ac = sc.nextInt();// 5
-        int tc = sc.nextInt();// 7
-        int nu = sc.nextInt();// 13
-        int ret = 0;
+        Long ac = sc.nextLong();// 5
+        Long tc = sc.nextLong();// 7
+        Long nu = sc.nextLong();// 13
+        Long ret;
         ret = (ac * 5) + (tc * 7) + (nu * 13);
         if (nu != 0) {
             ret = ret - 13;
         } else if (tc != 0) {
             ret = ret - 7;
         } else if (ac != 0) {
-            ret = ret - 5;
+            ret = ret - 5; 
+        } else {
+            System.out.println("0");
         }
         System.out.println(ret);
         sc.close();
