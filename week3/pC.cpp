3c5a4622464606cc336d934d34c1d7a232224572
4rn4u <vilx00@protonmail.com>
2024-05-23 14:17:00 +0200

    pC

--- week3/pC.cpp
+++ week3/pC.cpp
@@ -4,16 +4,27 @@ using namespace std;
 int main(){
     int n1, n2;
     cin >> n1 >> n2;
-    int arr1[n1][n2];
+    bool a[n1], b[n2];
+    char arr[n1][n2];
+    memset(a, 0, sizeof(a));
+    memset(b, 0, sizeof(b));
     for(int i = 0; i<n1; ++i){
         for(int j = 0; j<n2; ++j){
-            cin >> arr1[i][j];
+            cin >> arr[i][j];
+            if (arr[i][j] == '#'){
+                a[i] = true;
+                b[j] = true;
+            }
         }
     }
     for(int i = 0; i<n1; ++i){
         for(int j = 0; j<n2; ++j){
-
+            if (a[i] || b[j]){
+                cout << '#';
+            } else {
+                cout << '.';
+            }
         }
+        cout << endl;
     }
-
 }
