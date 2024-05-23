7d7dddddea0b00d03dd4b1b2a457851f914e5b0c
4rn4u <vilx00@protonmail.com>
2024-05-23 15:11:29 +0200

    problems

---
+++ week3/pD.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n1, n2, ret, filas;
+    cin >> n1 >> n2;
+    int columnas[n1];
+    memset(columnas, 0, sizeof(columnas));
+    for (int i = 0; i<n1; ++i){
+        for (int j = 0; j<n2; ++j){
+            cin >> arr1[i][j];
+        }
+    }
+    for (int i = 0; i<n1; ++i){
+        for (int j = 0; j<n2; ++j){
+            if (i == 0)
+            ret += arr1[i][j];
+        }
+    }
+
+}
