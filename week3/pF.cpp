f7f429430bf008e800c8b77cfacd0c40ff651824
4rn4u <vilx00@protonmail.com>
2024-05-24 10:01:42 +0200

    week4

--- week3/pF.cpp
+++ week3/pF.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int n1, n2;
     cin >> n1 >> n2;
-    int arr[n1][n2];
+    int arr[n2][n1];
     for(int i = 0; i < n1; i++){
         for(int j = 0; j < n2; j++){
             cin >> arr[j][n1-i-1];
