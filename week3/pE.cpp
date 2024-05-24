d81aa55ba0948986270496e75dcfe351acb4a027
4rn4u <vilx00@protonmail.com>
2024-05-24 09:28:49 +0200

    asd

--- week3/pE.cpp
+++ week3/pE.cpp
@@ -4,19 +4,22 @@ using namespace std;
 int main(){
     int n1,n2;
     cin>>n1>>n2;
-    int arr1[n1][n2], arr2[n2][n1];
+    char arr1[n1][n2];
     for (int i = 0; i<n1; ++i){
         for (int j = 0; j<n2; ++j){
             cin>>arr1[i][j];
         }
     }
-    for(int i = 0; i<n2; ++i){
-        for(int j = 0; j<n1; ++j){
-            arr2[j][i] = arr1[i][j];
-            cout << arr2[j][i] << " ";
+    for(int i = 0; i<n1 - 1; ++i){
+        for(int j = 0; j<n2 - 1; ++j){
+            if (arr1[i][j] == arr1[i+1][j] && arr1[i][j] == arr1[i][j+1] && arr1[i][j] == arr1[i+1][j+1]){
+                cout << "NO ORIGINAL";
+                return 0;
+            }
         }
-        cout << endl;
+
     }
+    cout << "ORIGINAL";
 
 
 }
