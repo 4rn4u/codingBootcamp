3462d57719738bc17d5274091accac625a2ff4d2
4rn4u <vilx00@protonmail.com>
2024-05-24 09:52:17 +0200

    problem

--- week3/pF.cpp
+++ week3/pF.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main(){
-
+    int n1, n2;
+    cin >> n1 >> n2;
+    int arr[n1][n2];
+    for(int i = 0; i < n1; i++){
+        for(int j = 0; j < n2; j++){
+            cin >> arr[j][n1-i-1];
+        }
+    }
+    for(int i = 0; i < n2; i++){
+        for(int j = 0; j < n1; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << '\n';
+    }
 }
